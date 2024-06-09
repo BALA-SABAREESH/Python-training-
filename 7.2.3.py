c9bf4a61e4d87eb04a45dde236e2acd0bfaf80f9
BALA-SABAREESH <sabareesh.0606@gmail.com>
2024-06-09 18:23:32 +0530

    Create 7.2.3.py

---
+++ 7.2.3.py
@@ -0,0 +1,8 @@
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+index = 0
+for char in a:
+    b += char
+    index += 1
+print("\nAfter copy the First string:", b)
+print("Second string:",b) 
